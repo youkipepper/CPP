e606a956e6c58b4bae628f729473410a5ac0cf57
youki.cui <youkipepper@gmail.com>
2022-08-18 01:51:36 +0800

    1.8.18

--- c++up/5.3.2-常用查找算法-random_shuffle.cpp
+++ c++up/5.3.2-常用查找算法-random_shuffle.cpp
@@ -1,7 +1,33 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <algorithm>
 using namespace std;
-#include<vector>
-#include<algorithm>
-#include<functional>
+#include <ctime>
 
-void test01() 
+void myPrint(int val)
+{
+    cout << val << " ";
+}
+
+void test01()
+{
+    srand((unsigned int)time(NULL));
+
+    vector<int> v;
+    for (int i = 0; i < 10; i++)
+    {
+        v.push_back(i);
+    }
+
+    random_shuffle(v.begin(), v.end());
+
+    for_each(v.begin(), v.end(), myPrint);
+}
+
+int main()
+{
+    test01();
+
+    return 0;
+}
