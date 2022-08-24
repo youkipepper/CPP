81765b8f6b2263fad7991d8b05a7e50dc9f467cc
Checkboxy <60711810+youkipepper@users.noreply.github.com>
2022-08-25 01:01:20 +0800

    -

--- Algorithm-cpp/bubblesort.cpp
+++ Algorithm-cpp/bubblesort.cpp
@@ -2,12 +2,27 @@
 #include <algorithm>
 using namespace std;
 
-void bubblesort1A(int A[], int n)//冒泡排序算法(版本1A)
+void bubblesort1A(int A[], int n) //冒泡排序算法(版本1A)
 {
-    bool sorted =false;
+    bool sorted = false; //整体排序标志, 假定尚未排序
+    while (!sorted)
+    {
+        sorted = true; //假定已经排序
+        for (int i = 1; i < n; i++)
+        {
+            if (A[i - 1])
+            {
+                swap(A[i - 1], A[i]);
+                sorted = false;
+            }
+        }
+        n--;
+    }
 }
 
 int main()
 {
+    int ArrayTest[]={9,3,5,1,3};
+    bubblesort1A(ArrayTest,3);
     return 0;
 }
